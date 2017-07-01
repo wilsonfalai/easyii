a29d4961c6b314f002ee525cb46d01c4d2d05f7c
Wilson Guimarães <wilsonfalai@gmail.com>
2017-07-01 13:03:29 -0300

    primeiro commit

---
+++ messages/pt/install.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'Installation' => 'Instalação',
+    'Install' => 'Instalar',
+    'Root password' => 'Root password',
+    'Admin E-mail' => 'Admin E-mail',
+    'Robot E-mail' => 'Robot E-mail',
+    'Auth time' => 'Auth time',
+    'Frontend toolbar position' => 'Frontend toolbar position',
+    'Password to login as root' => 'Password papra login como root',
+    'Used as "ReplyTo" in mail messages' => 'Used as "ReplyTo" in mail messages',
+    'Used as "From" in mail messages' => 'Used as "From" in mail messages',
+    'Required for using captcha in forms (guestbook, feedback)' => 'Required for using captcha in forms (guestbook, feedback)',
+    'You easily can get keys on' => 'Você pode facilmente obter chaves',
+    'ReCaptcha website' => 'ReCaptcha website',
+    'Installation completed' => 'Instalação completa',
+    'Installation error' => 'Erro de instalação',
+    'Cannot connect to database. Please configure `{0}`.' => 'Cannot connect to database. Please configure `{0}`.',
+    'EasyiiCMS is already installed. If you want to reinstall easyiiCMS, please drop all tables with prefix `easyii_` from your database manually.' => 'EasyiiCMS já está instalado. Se você quer reinstalar easyiiCMS, por favor delete todas as tabelas com prefixo `easyii_` de seu banco de dados manualmente.'
+];
